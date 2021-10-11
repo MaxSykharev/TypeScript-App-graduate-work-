3fb8bf0320dc2a8b98a6e64e3bf79e5f0cf71ef5
MAX SYKHAREV <maxsykharev@gmail.com>
2021-10-11 19:54:58 +0300

    adde albums

--- graduate/src/pages/AlbomsPage/index.tsx
+++ graduate/src/pages/AlbomsPage/index.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Table } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 
 export const Albums = () => {
+    const [album, setAlbum] = useState<any>(null);
+    const getRequest = () => {
+        fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(res => console.log(res.json()))
+        .catch(err => console.log(err))
+        }
+    useEffect(() => {
+        getRequest()
+      }, []);
     return (
-        <div>
-            albums
-        </div>
+        <Table
+      className="components-table-demo-nested"
+    //   columns={columns}
+    //   expandable={{ expandedRowRender }}
+    //   dataSource={data}
+    />
     )
 }
--- graduate/src/pages/usersPage/index.tsx
+++ graduate/src/pages/usersPage/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState} from "react";
-import { List } from 'antd';
+import { List, Avatar} from 'antd';
+import { resolve } from "dns";
 
 // Interface IUsers {
 //     id: Number;
@@ -11,30 +12,27 @@ export const UsersList = () => {
   const [user, setUser] = useState<any>(null);
     const getRequest =  () => {
       fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => setUser(res.json()))
-  .catch(err => console.log(err))
-  console.log(setUser);
-  
+      .then(res => console.log(res.json()))
+      .catch(err => console.log(err))
     }
   useEffect(() => {
     getRequest()
   }, []);
  
     return (
-        <div>
-              <List
-    itemLayout="horizontal"
-    // dataSource = {}
-    renderItem={item => (
-      <List.Item>
-        <List.Item.Meta
-          title={<a href=""></a>}
-          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-        />
-      </List.Item>
-    )}
-  />
-        </div>
+      <List
+      itemLayout="horizontal"
+      // dataSource={res}
+      renderItem={item => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+            title={<a href="https://ant.design">пав</a>}
+            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+          />
+        </List.Item>
+      )}
+    />
     )
-    }
+      }
 
